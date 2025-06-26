b9be4800d85e9a6f1468ca0dba2ad77ecf73094c
phil <uniphil@gmail.com>
2025-06-25 20:56:15 -0400

    wip, prompting flow with cookie

--- who-am-i/src/server.rs
+++ who-am-i/src/server.rs
@@ -2,10 +2,11 @@ use atrium_api::agent::SessionManager;
 use atrium_oauth::CallbackParams;
 use axum::{
     Router,
-    extract::{Query, State},
+    extract::{FromRef, Query, State},
     response::{Html, Redirect},
     routing::get,
 };
+use axum_extra::extract::cookie::{Cookie, Key, SignedCookieJar};
 
 use serde::Deserialize;
 use std::sync::Arc;
@@ -14,17 +15,20 @@ use tokio_util::sync::CancellationToken;
 
 use crate::{Client, authorize, client};
 
-const INDEX_HTML: &str = include_str!("../static/index.html");
 const FAVICON: &[u8] = include_bytes!("../static/favicon.ico");
+const INDEX_HTML: &str = include_str!("../static/index.html");
+const LOGIN_HTML: &str = include_str!("../static/login.html");
 
 pub async fn serve(shutdown: CancellationToken) {
     let state = AppState {
+        key: Key::generate(), // TODO: via config
         client: Arc::new(client()),
     };
 
     let app = Router::new()
         .route("/", get(|| async { Html(INDEX_HTML) }))
         .route("/favicon.ico", get(|| async { FAVICON })) // todo MIME
+        .route("/prompt", get(prompt))
         .route("/auth", get(start_oauth))
         .route("/authorized", get(complete_oauth))
         .with_state(state);
@@ -41,7 +45,23 @@ pub async fn serve(shutdown: CancellationToken) {
 
 #[derive(Clone)]
 struct AppState {
+    pub key: Key,
     pub client: Arc<Client>,
+}
+
+impl FromRef<AppState> for Key {
+    fn from_ref(state: &AppState) -> Self {
+        state.key.clone()
+    }
+}
+
+async fn prompt(jar: SignedCookieJar) -> (SignedCookieJar, Html<String>) {
+    let m = if let Some(did) = jar.get("did") {
+        format!("oh i know you: {did}")
+    } else {
+        LOGIN_HTML.into()
+    };
+    (jar, Html(m))
 }
 
 #[derive(Debug, Deserialize)]
@@ -51,21 +71,24 @@ struct BeginOauthParams {
 async fn start_oauth(
     State(state): State<AppState>,
     Query(params): Query<BeginOauthParams>,
-) -> Redirect {
-    let AppState { client } = state;
-    let BeginOauthParams { handle } = params;
-    let auth_url = authorize(&client, &handle).await;
-    Redirect::to(&auth_url)
+    jar: SignedCookieJar,
+) -> (SignedCookieJar, Redirect) {
+    // if any existing session was active, clear it first
+    let jar = jar.remove("did");
+
+    let auth_url = authorize(&state.client, &params.handle).await;
+    (jar, Redirect::to(&auth_url))
 }
 
 async fn complete_oauth(
     State(state): State<AppState>,
     Query(params): Query<CallbackParams>,
-) -> Html<String> {
-    let AppState { client } = state;
-    let Ok((oauth_session, _)) = client.callback(params).await else {
+    jar: SignedCookieJar,
+) -> (SignedCookieJar, Html<String>) {
+    let Ok((oauth_session, _)) = state.client.callback(params).await else {
         panic!("failed to do client callback");
     };
     let did = oauth_session.did().await.expect("a did to be present");
-    Html(format!("sup: {did:?}"))
+    let jar = jar.add(Cookie::new("did", did.to_string()));
+    (jar, Html(format!("sup: {did:?}")))
 }
